5fbd786a228d18516b5e5a802d9840dea2223de7
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2024-09-23 09:10:27 -0300

    Corrindo nome da aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'EuroIntegrate',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
         colorScheme: ColorScheme.fromSeed(seedColor: azulEuro),
